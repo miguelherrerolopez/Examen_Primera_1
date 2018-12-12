4917bc1d10ea7b22b1831de8412444820ad2d195
miguelherrerolopez <miguelherrerolopez2017@gmail.com>
2018-12-12 20:44:01 +0100

    Initial commit

---
+++ Examen_Primera_2/Assets/BOLA_2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOLA_2 : MonoBehaviour {
+
+    public string nombre;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        void OnTriggerEnter(Collider collider)
+        {
+            Destroy(this);
+        }
+
+    }
+}
